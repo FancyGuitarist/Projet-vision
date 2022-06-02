a298d2b132c90563b209c4f4095bca8499b9ef24
Apple <>
2022-06-02 21:29:16 +0000

    Initial release.

---
+++ ContinuityCam/ContinuityCamApp.swift
@@ -0,0 +1,20 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A sample app that shows how to use Continuity Camera in macOS.
+*/
+
+import SwiftUI
+
+@main
+struct ContinuityCamApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .navigationTitle("Continuity Camera Sample")
+                .frame(minWidth: 800, minHeight: 600)
+        }
+    }
+}
